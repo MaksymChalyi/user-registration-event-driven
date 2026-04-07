eaad1a61d46a0b267ffcf2adcf8d4c606c1615b6
Maksym Chalyi <maksymchalyi2004@gmail.com>
2026-04-08 00:46:47 +0300

    init

---
+++ user-service/src/main/java/com/maksimkaxxl/userregistrationeventdriven/UserRegistrationEventDrivenApplication.java
@@ -0,0 +1,13 @@
+package com.maksimkaxxl.userregistrationeventdriven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationEventDrivenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegistrationEventDrivenApplication.class, args);
+    }
+
+}
---
+++ user-service/src/test/java/com/maksimkaxxl/userregistrationeventdriven/UserRegistrationEventDrivenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maksimkaxxl.userregistrationeventdriven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationEventDrivenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
